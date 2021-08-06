f3aa5061b5649c0985ee3fb3082dcf1b015a1785
dododcdc <dcdc3166@foxmail.com>
2021-08-06 18:09:08 +0800

    update

--- src/main/java/com/action/demoaction/DemoAction.java
+++ src/main/java/com/action/demoaction/DemoAction.java
@@ -1,6 +1,5 @@
 package com.action.demoaction;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +8,6 @@ public class DemoAction {
 
     @GetMapping("/action")
     public String doAction() {
-        return "action";
+        return "xxxxxxxxx";
     }
 }
