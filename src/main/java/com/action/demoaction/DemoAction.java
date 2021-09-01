bdcdee84f41d3ee902fa704657edfb6da5525257
dododcdc <dcdc3166@foxmail.com>
2021-09-01 09:21:18 +0800

    update

--- src/main/java/com/action/demoaction/DemoAction.java
+++ src/main/java/com/action/demoaction/DemoAction.java
@@ -1,14 +1,22 @@
 package com.action.demoaction;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class DemoAction {
 
+    @ResponseBody
     @GetMapping("/urls")
     public String urls() {
         return "https://goodbin.cn/videos/v4.mp4";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
     }
 
 }
