b72ec86323ef4d46d636956d299133cfbb546d2f
dododcdc <dcdc3166@foxmail.com>
2021-09-24 15:05:11 +0800

    update jack

--- src/main/java/com/action/demoaction/Foo.java
+++ src/main/java/com/action/demoaction/Foo.java
@@ -5,5 +5,6 @@ public class Foo implements IFoo{
     public void say() {
         System.out.println("today is good day");
         System.out.println("today is good day too");
+        System.out.println("hello jack");
     }
 }
