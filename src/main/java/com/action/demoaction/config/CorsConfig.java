0ed7dd608a351468c0a55b580f96d26d2bc3a8a6
dododcdc <dcdc3166@foxmail.com>
2021-10-01 04:02:57 +0800

    update

--- src/main/java/com/action/demoaction/config/CorsConfig.java
+++ src/main/java/com/action/demoaction/config/CorsConfig.java
@@ -1,8 +1,10 @@
 package com.action.demoaction.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
