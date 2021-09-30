84b455c170ff12b74b317248fb4419d8c623a179
dododcdc <dcdc3166@foxmail.com>
2021-10-01 04:02:16 +0800

    update

---
+++ src/main/java/com/action/demoaction/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package com.action.demoaction.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
+                .maxAge(3600 * 24);
+    }
+}
